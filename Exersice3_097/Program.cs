bb4135ecb11b1e34277ac1df6b890cab0fe8026c
ZaqyMunif <zaqymunif17@gmail.com>
2022-11-25 16:16:38 +0700

    add variable in traverse

--- Exersice3_097/Program.cs
+++ Exersice3_097/Program.cs
@@ -47,6 +47,22 @@ namespace Exersice3_097
         public void traverse()/*Traverses all the nodes of the list*/
         {
 
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+            {
+                Console.WriteLine("\nRecord in the list are:\n");
+                Node currentNode;
+                currentNode = LAST.next;
+                while (currentNode != LAST)
+                {
+                    Console.Write(currentNode.rollNumber + "   " +
+                        currentNode.name + "\n");
+                    currentNode = currentNode.next;
+                }
+                Console.Write(LAST.rollNumber + "   " + LAST.name + "\n");
+            }
         }
+        
     }
 }

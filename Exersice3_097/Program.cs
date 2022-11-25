ae7735c6483e17fd5f59985fa5ab90e7b8f47aad
ZaqyMunif <zaqymunif17@gmail.com>
2022-11-25 16:04:27 +0700

    Add project files.

---
+++ Exersice3_097/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exersice3_097
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
